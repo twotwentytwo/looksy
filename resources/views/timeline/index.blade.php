0df269470fc68632ce07b7995a4e8a6f2a21e715
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-09-26 22:33:46 +0100

    adding new position of timing

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -15,7 +15,7 @@
 	            		<div class="details">
 	            			<p class="source"><a href="{{ route('pick.index', ['statusId' => $status->id]) }}">{{ $status->source }}</a></p>
 	            			<p class="title"><a href="{{ route('pick.index', ['statusId' => $status->id]) }}">{{ $status->title }}</a></p>
-	            			<p class="timing">{{ $status->created_at->diffForHumans() }}</p>
+	            			
 	            		</div>
 	            	</div>
 	        		<div class="media">
@@ -24,6 +24,7 @@
 					    </a>
 					    <div class="media-body">
 					        <h4 class="media-heading"><a href="{{ route('profile.index', ['username' => $status->user->username]) }}">{{ $status->user->getNameOrUsername() }}</a></h4>
+					        <p>{{ $status->created_at->diffForHumans() }}</p>
 					    </div>
 					</div>
 	        	@endforeach
