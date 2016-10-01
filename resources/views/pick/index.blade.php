185cb93df5771889d3329f0938422cc7814ba590
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-01 16:43:14 +0100

    styling comments

--- resources/views/pick/index.blade.php
+++ resources/views/pick/index.blade.php
@@ -70,17 +70,12 @@
 
                     @foreach($status->replies as $reply)
                         <div class="media">
-                            <a class="pull-left" href="{{ route('profile.index', ['username' => $reply->user->username]) }}">
-                                <img class="media-object  profile-image" alt="{{ $reply->user->getNameOrUsername() }}" src="{{ $reply->user->getAvatarUrl() }}">
-                            </a>
+                           <div class="comment">
                             <div class="media-body">
                                 <h5 class="media-heading"><a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">{{ $reply->user->getNameOrUsername() }}</a></h5>
                                 <p>{{ $reply->body }}</p>
-                                <ul class="list-inline">
-                                    <li>{{ $reply->created_at->diffForHumans() }}</li>
-                                     <!--<li><a href="#">Like</a></li>
-                        <li>10 likes</li>-->
-                                </ul>
+                                <p class="timing">{{ $reply->created_at->diffForHumans() }}</p>
+                            </div>
                             </div>
                         </div>
                     @endforeach
