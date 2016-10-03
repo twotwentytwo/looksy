f04d87625dade33448863ebdbaa22c32eed81f29
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-03 18:07:33 +0100

    changing sign up message

--- resources/views/emails/sendtofriend.blade.php
+++ resources/views/emails/sendtofriend.blade.php
@@ -1,3 +1,3 @@
 Hello there {{ $email }}, someone thinks you'll like Pick List - a new sharing app for you and your friends to share your top cultural picks. 
 
-Check out http://middletonprototype.com and start sharing today.
+Check out http://middletonprototype.com and start recommending your top picks today.
