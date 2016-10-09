a7e7fdc15e67a6194fc0df43bd05459e11d43aaf
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-09 11:34:40 +0100

    fixing friend request

--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -3,12 +3,13 @@
 		<li class="home"><a href="{{ route('home') }}">Home</a></li>
 		<li class="search"><a href="{{ route('search.index') }}">Serach</a></li>
 		<li class="add"><a href="{{ route('add.index') }}">Add</a></li>
-		<li class="friends"><a href="{{ route('friend.index') }}">Friends</a></li>
+		<li class="friends">
+            <a href="{{ route('friend.index') }}">Friends</a>
+            @if(Auth::user()->friendRequests()->count())
+                <span class="friend-requests"></span>
+            @endif
+        </li>
 		<li class="profile"><a href="{{ route('profile.index', ['username' => Auth::user()->username]) }}">Profile</a></li>
     </ul>
-    @if(Auth::user()->friendRequests()->count())
-    	<div class="friend-requests">
-    		<p class="count">{{ Auth::user()->friendRequests()->count() }}</p>
-    	</div>
-    @endif
+    
 </div>
