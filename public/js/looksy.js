b6828d4a251e696b3cca7d3a4c17829acfe3c0d2
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-09 16:30:45 +0100

    adding preview

--- public/js/looksy.js
+++ public/js/looksy.js
@@ -1,1 +1,17 @@
-//$('.scroll').jscroll();
+$(".add-form .add").change(function() {
+	
+	var url = $(".add-form .add").val();
+	
+	var urlEncoded = encodeURIComponent(url);
+	
+	var requestUrl = 'http://opengraph.io/api/1.0/site/' + urlEncoded;
+
+	$.getJSON(requestUrl, function(json) {	
+		
+		//$('.preview .explainer').hide();
+		$('.preview').show();
+		//$('.preview .title').show();
+		$('.preview .title').text(json.hybridGraph.title);
+		$('.preview img').attr('src', json.hybridGraph.image);
+	});   
+});
--- resources/views/add/index.blade.php
+++ resources/views/add/index.blade.php
@@ -22,45 +22,58 @@
  			<div class="row">
 			    <div class="col-lg-6">
 			    	
-			        <form role="form" action="{{ route('status.post') }}" method="post">
+			        <form role="form" action="{{ route('status.post') }}" method="post" class="add-form">
+                <h3>Paste in a URL</h3>
+                <p>Share something to inspire your friends.</p>
 			            <div class="form-group{{ $errors->has('status') ? ' has-error' : '' }}">
-			                <input placeholder="Paste in a URL here" name="status" class="form-control add">
+			                <input placeholder="URL goes here" name="status" class="form-control add">
                       @if($errors->has('status'))
                         <span class="help-block">{{ $errors->first('status') }}</span>
                       @endif
                   </div>
-                  <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
-                    <select name="type">
-                      
-                      <option value="">Choose category</option>
-                      <option value="film">Film</option>
-                      <option value="tv">TV show</option>
-                      <option value="video">Video clip</option>
-                      <option value="music">Music</option>
-                      <option value="podcast">Podcast</option>
-                      <option value="web">Web</option>
-                      <option value="news">News</option>
-                      <option value="app">Mobile app</option>
-                      <option value="food">Food &amp; drink</option>
-                      <option value="art">Art</option>
-                      <option value="book">Book / Writing</option>
-                      <option value="shop">Shop</option>
-                      <option value="location">Location</option>
 
-                    </select>
-                    @if($errors->has('category'))
-                      <span class="help-block">{{ $errors->first('category') }}</span>
-                    @endif
+                  <div class="preview">
+                    <h3>Preview</h3>
+                    <img src="" class="image" />
+                    <p class="title"></p>
                   </div>
-                  <div class="form-group{{ $errors->has('review') ? ' has-error' : '' }}">
-                    <textarea placeholder="Add a mini review" name="review" class="form-control add" rows="2"></textarea>
-                    @if($errors->has('review'))
-                      <span class="help-block">{{ $errors->first('review') }}</span>
-                    @endif
-                  </div>    
-                  <p class="add-strapline">Why not post something to inspire your friends?</p>
-                  <p class="add-strapline">Good things happen when we share.</p>
-			            <button type="submit" class="btn btn-default">Share</button>
+
+                    <h3>Add details</h3>
+                    <p>Add a category and a review for your friends.</p>
+
+                    <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
+                      <select name="type">
+                        
+                        <option value="">Choose category</option>
+                        <option value="film">Film</option>
+                        <option value="tv">TV show</option>
+                        <option value="video">Video clip</option>
+                        <option value="music">Music</option>
+                        <option value="podcast">Podcast</option>
+                        <option value="web">Web</option>
+                        <option value="news">News</option>
+                        <option value="app">Mobile app</option>
+                        <option value="food">Food &amp; drink</option>
+                        <option value="art">Art</option>
+                        <option value="book">Book / Writing</option>
+                        <option value="shop">Shop</option>
+                        <option value="location">Location</option>
+
+                      </select>
+                      @if($errors->has('category'))
+                        <span class="help-block">{{ $errors->first('category') }}</span>
+                      @endif
+                    </div>
+                    <div class="form-group{{ $errors->has('review') ? ' has-error' : '' }}">
+                      <textarea placeholder="Add a mini review" name="review" class="form-control add" rows="2"></textarea>
+                      @if($errors->has('review'))
+                        <span class="help-block">{{ $errors->first('review') }}</span>
+                      @endif
+                    </div>
+                    <button type="submit" class="btn btn-default">Share</button>
+                  
+                  
+			            
 			            <input type="hidden" name="_token" value="{{ Session::token() }}">
 			        </form>
 			        <hr>
