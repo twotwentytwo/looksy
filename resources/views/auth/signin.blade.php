b0efdcfde2979fd2635bfbf7b47fe5b861055c4e
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-28 20:33:13 +0100

    adding manifest and icons for progressive web app

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -6,6 +6,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+		<!-- Disable tap highlight on IE -->
+		<meta name="msapplication-tap-highlight" content="no">
+
+		<!-- Web Application Manifest -->
+		<link rel="manifest" href="manifest.json">
+
+		<!-- Add to homescreen for Chrome on Android -->
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="application-name" content="Pick List">
+		<link rel="icon" sizes="192x192" href="img/touch/chrome-touch-icon-192x192.png">
+
+		<!-- Add to homescreen for Safari on iOS -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="apple-mobile-web-app-title" content="Web Starter Kit">
+		<link rel="apple-touch-icon" href="img/touch/apple-touch-icon.png">
+
+		<!-- Tile icon for Win8 (144x144 + tile color) -->
+		<meta name="msapplication-TileImage" content="img/touch/ms-touch-icon-144x144-precomposed.png">
+		<meta name="msapplication-TileColor" content="#ff8d6b">
+
+		<!-- Color the status bar on mobile devices -->
+		<meta name="theme-color" content="#ff8d6b">
         <!-- Looksy css  -->
 
         <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
