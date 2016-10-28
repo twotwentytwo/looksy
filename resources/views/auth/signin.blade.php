8a2a0cef9f9b4d79366b196b25316993534ff7d8
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-10-28 21:41:13 +0100

    adding manifest andoffline and service worker

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -72,6 +72,7 @@
     <!-- Latest compiled and minified JavaScript -->
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	  <script src="{{asset('js/looksy.js')}}"></script>
+	  <script src="{{asset('js/service-worker.js')}}"></script>
 
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
