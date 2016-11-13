ef8bc074ec297a7351fe376894e34fbc18e7025d
Tom Kershaw <tom@twotwentytwo.co.uk>
2016-11-13 13:16:53 +0000

    fix comments and review length

--- public/js/looksy.js
+++ public/js/looksy.js
@@ -1,12 +1,12 @@
-$(".add-form .add-input").change(function() {
+$(".add-input").change(function() {
 	
 	var url = $(".add-form .add-input").val();
-
-
-	url_validate = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	var url_validate = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 	
 	if(!url_validate.test(url)) {
+
 		$( ".add-input" ).after( "<span class='help-block'>Oooops... you need to enter a valid URL for your Pick.</span>" );
+	
 	} else {
 	   	
 	   	var urlEncoded = encodeURIComponent(url);
