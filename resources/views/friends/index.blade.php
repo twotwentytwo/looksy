56c63e1226c7d9d8008f7b55e4640b3556eedb2d
Tom Kershaw <tom@twotwentytwo.co.uk>
2017-01-09 20:51:35 +0000

    fix filter menu

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -15,6 +15,20 @@
 @section('content')
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
+
+            <div class="friend-block">
+                
+                <form role="search" action="{{ route('search.results') }}">
+                    <div class="form-group{{ $errors->has('query') ? ' has-error' : '' }}">
+                        <input type="text" name="query" class="form-control" placeholder="Search friends">
+                        @if($errors->has('query'))
+                            <span class="help-block">{{ $errors->first('query') }}</span>
+                        @endif
+                    </div>
+                    <!--<button type="submit" class="btn btn-default">Search</button>-->
+                </form>
+            </div>
+
             <div class="friend-block">
                 @if (!$friends->count())
                     <p>You don't have any friends yet.</p>
@@ -34,7 +48,7 @@
             </div>
             
             <div class="friend-block requests">
-            <h3>Friend requests</h3>
+                <h3>Friend requests</h3>
                 @if (!$requests->count())
                     <p>You have no friend requests.</p>
                 @else
@@ -49,35 +63,6 @@
                 @endif
             </div>
 
-            <div class="friend-block">
-                <h3>Search friends</h3>
-                <form role="search" action="{{ route('search.results') }}">
-                    <div class="form-group{{ $errors->has('query') ? ' has-error' : '' }}">
-                        <input type="text" name="query" class="form-control" placeholder="Enter friends name">
-                        @if($errors->has('query'))
-                            <span class="help-block">{{ $errors->first('query') }}</span>
-                        @endif
-                    </div>
-                    <button type="submit" class="btn btn-default">Search</button>
-                </form>
-            </div>
-
-            
-            <!--
-            <div class="friend-block">
-                <h3>Invite friends</h3>
-                <form role="form" action="{{ route('emails.sendtofriend') }}" method="post">
-                    <div class="form-group{{ $errors->has('invite') ? ' has-error' : '' }}">
-                        <input placeholder="Type in friends email here" name="invite" class="form-control add">
-                        @if($errors->has('invite'))
-                            <span class="help-block">{{ $errors->first('invite') }}</span>
-                        @endif
-                    </div>
-                    <button type="submit" class="btn btn-default">Invite</button>
-                    <input type="hidden" name="_token" value="{{ Session::token() }}">
-                </form>
-            </div>
-            -->
         </div>
     </div>
 @stop  
