888483ade3b0904c0ece7d1bce502a368389beff
Tom Kershaw <tom@twotwentytwo.co.uk>
2017-01-25 19:06:51 +0000

    404 page and js edits

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,10 +8,6 @@
   error-404
 @stop
 
-@section('navigation')
-    @include('templates.partials.navigation')
-@stop
-
 @section('content')
   <p>There seems to have been an error.</p>
   <p>Please try <a href="{{ route('home') }}">logging in again</a></p>
