1adfcbac708e30142c08aeaa2dc36ae48d82b7fc
Tom Kershaw <tom@twotwentytwo.co.uk>
2017-03-19 17:25:43 +0000

    editing terms

--- resources/views/profile/terms.blade.php
+++ resources/views/profile/terms.blade.php
@@ -14,20 +14,16 @@
 
 @section('content')
 
-<h3>Last updated: 25.01.17</h3>
-<p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the http://www.picklist.co.uk website and the PickList mobile application (the "Service") operated by PickList ("us", "we", or "our").</p>
+<h3>Last updated: 18.03.17</h3>
+<p>Please read these Terms and Conditions ('Terms', 'Terms and Conditions') carefully before using the http://www.picklist.co.uk website and the PickList mobile application (the "Service") operated by PickList ("us", "we", or "our").</p>
 <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
 
-<h3>Purchases</h3>
-<p>If you wish to purchase any product or service made available through the Service ("Purchase"), you may be asked to supply certain information relevant to your Purchase including, without limitation, your ...</p>
-<p>The Purchases section is for businesses that sell online (physical or digital). For the full disclosure section, create your own Terms and Conditions.</p>
-
 <h3>Content</h3>
-<p>Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the... The Content section is for businesses that allow users to create, edit, share, make content on their websites or apps. For the full disclosure section, create your own Terms and Conditions.</p>
+<p>Our service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the content you post.</p>
 
 <h3>Links To Other Web Sites</h3>
 <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by PickList.</p>
-<p>My Company (change this) has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that My Company (change this) shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+<p>PickList has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that PickList shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
 
 <h3>Changes</h3>
 <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
